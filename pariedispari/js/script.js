be847a7b38b4d497b5cff973daeb32b077fc0885
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-18 17:15:46 +0100

    fine pari e dispari

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -10,7 +10,26 @@ function pari(value) {
 	else
 		return false;
 }
+
+var domanda;
+var button = confirm("Pari?");
+if (button === true) {
+  domanda = true;
+} else {
+  domanda = false;
+}
+
 var numeroUtente = parseInt(prompt('Inserisci un numero compreso tra 1 e 5'));
 console.log(numeroUtente);
 var numeroComputer = Math.floor(Math.random() * 5) + 1  ;
 console.log(numeroComputer);
+var sommaNumeri = numeroUtente + numeroComputer;
+console.log(sommaNumeri);
+var even = pari(sommaNumeri);
+console.log(even);
+
+if (even == true && domanda==true) {
+ document.getElementById('risultato').innerHTML = 'Hai vinto'
+} else {
+  document.getElementById('risultato').innerHTML = 'Hai perso'
+}
