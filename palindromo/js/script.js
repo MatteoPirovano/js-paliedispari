010252902d0b6ceed9e8225a2266f4d8ac45e14f
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-18 18:45:09 +0100

    fine

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -1,15 +1,23 @@
 // Palindroma
 // Chiedere all’utente di inserire una parola.
 // Creare una funzione per capire se la parola inserita è palindroma.
+function palindroma(parola1,parola2) {
+  if (parola1 == parola2){
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// function palindromo (inserireParola) {
-//   parola == parola.reverse();
-//   return parola;
-// }
-// var res = [];
-// var parola = prompt('inserisci una parola');
-// var res = parola.split(' ');
-// var contrario = res.reverse();
-// var parola = palindromo(parola);
-// console.log(parola);
-// console.log(contrario);
+var parola = prompt ('insirisci una parola');
+var parolaCheck=' ';
+
+for ( i = parola.length-1; i >= 0; i-- ) {
+  parolaCheck += parola[i];
+}
+
+if (palindroma(parola, parolaCheck)==true) {
+  document.getElementById('paliparola').innerHTML = 'la tua parola è palindroma';
+} else {
+  document.getElementById('paliparola').innerHTML = 'la tua parola non è palindroma';
+};
--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -1,16 +1,10 @@
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri.
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
-// Dichiariamo chi ha vinto.
 function pari(value) {
-	if (value%2 == 0)
+	if (value%2 == 0){
 		return true;
-	else
+	} else {
 		return false;
+	}
 }
-
 function randomNumber(min, max)  {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -18,21 +12,21 @@ function randomNumber(min, max)  {
 }
 
 var domanda;
-var button = confirm("Pari?");
+var button = confirm("Pari (ok) dispari (annulla)?");
 if (button === true) {
   domanda = true;
+	alert('hai scelto PARI')
 } else {
   domanda = false;
+	alert('Hai scelto DISPARI')
 }
 
 var numeroUtente = parseInt(prompt('Inserisci un numero compreso tra 1 e 5'));
-console.log(numeroUtente);
 var numeroComputer = randomNumber(1,5);
-console.log(numeroComputer);
 var sommaNumeri = numeroUtente + numeroComputer;
-console.log(sommaNumeri);
+ document.getElementById('numero').innerHTML =sommaNumeri + '<br>'
 var even = pari(sommaNumeri);
-console.log(even);
+
 
 if (even == true && domanda==true) {
  document.getElementById('risultato').innerHTML = 'Hai vinto'
